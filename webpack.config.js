dbb23f4dd647b283d411b1a04eb3b6fc08528024
M.D. van Es <es@mdworld.nl>
2016-10-18 19:48:26 +0200

    Adding webpack for modules

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+var path = require('path');
+var merge = require('webpack-merge');
+var webpack = require('webpack');
+
+var common = {
+
+    entry: './src/app.js',
+
+
+
+    output: {
+        path: path.join(__dirname, 'public/js'),
+        filename: 'pwa.bundle.js'
+    },
+
+    devtool: 'source-map',
+
+    // Add minification
+    //plugins: [
+    //    new webpack.optimize.UglifyJsPlugin()
+    //],
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
+
+module.exports = common;
