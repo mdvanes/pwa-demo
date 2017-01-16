5d8ad174768550a4d39b2d448fe62925430da7b7
M.D. van Es <es@mdworld.nl>
2017-01-16 14:32:39 +0100

    Clean up, docs

--- src/serviceworker/cache.js
+++ src/serviceworker/cache.js
@@ -20,42 +20,20 @@ export function setInstall(caches) {
 
 export function setFetch(event) {
     // Parse the URL:
-    //const requestURL = new URL(event.request.url);
-    //console.log('%csw/cache:', 'color: green; font-weight: bold', 'Fetch event', requestURL, requestURL.pathname);
-    //
-    //// Handle requests to a particular host specifically
-    //if (requestURL.pathname === '/sendUptime') {
-    //    console.log('%csw/cache:', 'color: green; font-weight: bold', 'Intercepted sendUptime');
-    //    event.respondWith(fetch(event.request)
-    //        .catch(err => {
-    //            console.log('Cant reach /sendUptime', err);
-    //            console.log('%csw/cache:', 'color: green; font-weight: bold', 'Can\'t reach /sendUptime', err);
-    //            // TODO if this is even needed return caches.match(event.request);
-    //            return {'sendUptime': false, 'offline': true};
-    //        }));
-    //    return;
-    //}
-
-    // Parse the URL:
     const requestURL = new URL(event.request.url);
-    //console.log('Cache SW: fetch event', requestURL, requestURL.pathname);
-    console.log('%csw/cache:', 'color: green; font-weight: bold', 'Fetch event', requestURL, requestURL.pathname);
+    console.log('%csw/cache:', 'color: green; font-weight: bold', 'Fetch event', requestURL.pathname, requestURL.href);
 
     // Handle requests to a particular host specifically
     if (requestURL.pathname === '/sendUptime') {
-        console.log('%cCache SW:', 'color: green; font-weight: bold', 'intercepted sendUptime');
+        console.log('%csw/cache:', 'color: green; font-weight: bold', 'Intercepted sendUptime');
         event.respondWith(fetch(event.request)
-        //.then(res => console.log('result', res))
             .catch(function() {
-                console.warn('Cache SW: cant reach /sendUptime');
-                // TODO if this is even needed return caches.match(event.request);
-                //return {'sendUptime': false, 'offline': true};
-                //return new Response();
+                console.warn('%csw/cache:', 'color: green; font-weight: bold', 'Can\'t reach /sendUptime');
+                // To return cached response: caches.match(event.request);
                 var response = {'sendUptime': false, 'offline': true};
                 var responseBlob = new Blob([JSON.stringify(response)], {type : 'application/json'});
-                //var myBlob = new Blob();
-                var init = { 'status': 200 , 'statusText': 'ok' };
-                return new Response(responseBlob, init);
+                var responseInit = { 'status': 200 , 'statusText': 'ok' };
+                return new Response(responseBlob, responseInit);
             }));
         return;
     }
